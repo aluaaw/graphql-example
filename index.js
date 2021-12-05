504cd84259347c12abdef2b821f30beeb74165fd
aluaaw <ylua.aw@gmail.com>
2021-12-05 20:56:14 +0900

    쿼리 동작 방식 확인

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+// function -> typeDefs -> Resolvers
+
 const typeDefs = `
 type Book {
     title: String
     author: String
-  }  
+}
+type Title {
+    title: String
+}
+type Author {
+    author: String
+}
 type Query {
-  books: [Book]
+  bookObj: [Book],
+  titleArr: [Title],
+  authorArr: [Author]
 }
 `;
 
 // data
-const books = [
+const bookObj = [
     {
         title: 'title',
         author: 'author'
@@ -21,18 +31,36 @@ const books = [
         author: 'author1'
     }
 ];
+const titleArr = [
+    {
+        title: 'test'
+    },
+    {
+        title: 'test'
+    }
+]
+const authorArr = [
+    {
+        author: 'test'
+    },
+    {
+        author: 'test'
+    }
+]
 
 // resolvers
 const resolvers = {
     Query: {
-        books: () => books,
+        bookObj: () => bookObj,
+        titleArr: () => titleArr,
+        authorArr: () => authorArr
     },
 };
 
 // apollo-server constructor requires two parameters
 const server = new ApolloServer({typeDefs, resolvers});
 
-// listen method launches a web server.
+// listen method launches a web server
 server.listen().then(({url}) => {
     console.log(`Server ready at ${url}`);
 });
